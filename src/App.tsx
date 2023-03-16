91f0183ce56f9a33ff129f4f8367e9968960e5ab
Галкин Владислав <vladislav_galkin@f-lab.tech>
2023-03-16 14:46:02 +0500

    Let me listen Morgenshtern

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC, useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface respI {
+    tracks: []
+}
+
+const App: FC<respI> = () => {
+    const [loading, setLoading] = useState(true)
+    const [rand, setRand] = useState<any>()
+    const [resp, setResp] = useState<any>()
+    useEffect(() => {
+        fetch('artist.json', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(response => {
+                setResp(response)
+                setRand(response.tracks[Math.floor(Math.random() * 135)])
+                setLoading(false)
+            })
+    }, [])
+
+    const getTrack = () => {
+        setRand(resp.tracks[Math.floor(Math.random() * 135)])
+    }
+    return (
+        <div className="App">
+            <div className={"coolImg"}><img
+                src="https://i.pinimg.com/564x/aa/79/26/aa792605e63bc2e276bbdc1f4c7a6253.jpg" alt=""/></div>
+            <div className="getTrackContainer">
+                <button onClick={() => {
+                    getTrack()
+                }}>ХОЧЮ ТРЕК
+                </button>
+                <p>MORGENSHTERN - {!loading && rand.title}</p>
+                <div className="search">
+                    <button
+                        onClick={() => window.open(`https://www.youtube.com/results?search_query=MORGENSHTERN+ - +${rand.title}`)}>
+                        послушать???
+                    </button>
+                </div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/cdk4hVPN_F0?autoplay=1"
+                        title="YouTube video player" frameBorder="0"
+                        allow="autoplay"
+                        allowFullScreen>
+                </iframe>
+            </div>
+        </div>
+    );
 }
 
 export default App;
