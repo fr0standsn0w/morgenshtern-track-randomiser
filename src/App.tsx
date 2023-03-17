e250d89907775b564fdb3d3f4fbcfc26a9a02989
Галкин Владислав <vladislav_galkin@f-lab.tech>
2023-03-17 12:25:19 +0500

    Let me listen Morgenshtern

--- src/App.tsx
+++ src/App.tsx
@@ -7,51 +7,62 @@ interface RespI {
     children?: React.ReactNode
 }
 
+interface RandI {
+    title: null | string,
+    realId: null | string
+}
 
-
-const App: FC<RespI> = () => {
+const App: FC = () => {
     const [loading, setLoading] = useState(true)
-    const [rand, setRand] = useState<any>()
+    const [rand, setRand] = useState<RandI>()
     const [resp, setResp] = useState<RespI>(Object)
     const [listen, setListen] = useState<boolean>(false)
     useEffect(() => {
-        fetch('artist.json', {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-            },
-        })
-            .then(response => response.json())
-            .then(response => {
-                setResp(response)
-                setRand(response.tracks[Math.floor(Math.random() * 135)])
-                setLoading(false)
+        const setFetch = async () => {
+            await fetch('artist.json', {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                },
             })
+                .then(response => response.json())
+                .then(response => {
+                    setResp(response)
+                    setRand(response.tracks[Math.floor(Math.random() * 135)])
+                    setLoading(false)
+                })
+        }
+        setFetch()
     }, [])
     const getTrack = () => {
         setRand(resp.tracks[Math.floor(Math.random() * 135)])
     }
+
     return (
         <div className="App">
-            <div className={"coolImg"} style={listen ? {  animation: '2s infinite linear coolImgSpin'} : {}}><img
+            <div className={"coolImg"} style={listen ? {animation: '2s infinite linear coolImgSpin'} : {}}><img
                 src="https://i.pinimg.com/564x/aa/79/26/aa792605e63bc2e276bbdc1f4c7a6253.jpg" alt=""/></div>
+            {/*// @ts-ignore*/}
             <div className="getTrackContainer">
                 <button onClick={() => {
                     setListen(false)
                     getTrack()
                 }}>ХОЧЮ ТРЕК
                 </button>
-                <p>MORGENSHTERN - {!loading && rand.title}</p>
+                <p>MORGENSHTERN - {!loading && rand?.title}</p>
                 <div className="search">
                     <button
                         onClick={() => setListen(!listen)
-                    }>
+                        }>
                         послушать???
                     </button>
                 </div>
-                {listen && <iframe style={{border: 'none', width: '100%', height: '180px'}} width='100%' height='180'
-                                   allow='autoplay;'
-                                   src={`https://music.yandex.ru/iframe/#track/${!loading && rand.realId}/23812350`}></iframe>}
+
+                {listen &&
+                    <iframe style={{border: 'none', width: '100%', height: '180px'}} width='100%' height='180'
+                            allow='autoplay;' title="music"
+                            src={`https://music.yandex.ru/iframe/#track/${!loading && rand?.realId}/23812350`}></iframe>
+                }
             </div>
         </div>
     );
